de1d0a1e14924c2355dff0af25db70db545af529
kdodeja62@gmail.com <kdodeja62@gmail.com>
2024-01-02 07:10:31 +0530

    Add permalink

--- src/app/post/new-post/new-post.component.ts
+++ src/app/post/new-post/new-post.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-new-post',
   templateUrl: './new-post.component.html',
-  styleUrl: './new-post.component.css'
+  styleUrl: './new-post.component.css',
 })
 export class NewPostComponent {
+  permalink: string = '';
+  onTitleChange($event: any) {
+    const title = $event.target.value;
+    this.permalink = title.replace(/\s/g, '-');
 
+    
+  }
 }
