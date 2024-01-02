7d9f74d4eb6f93895be86fb0f468fa1b11ccaee9
kdodeja62@gmail.com <kdodeja62@gmail.com>
2024-01-03 02:10:54 +0530

    Post Category

--- src/app/post/new-post/new-post.component.ts
+++ src/app/post/new-post/new-post.component.ts
@@ -1,14 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CategoriesService } from '../../services/categories.service';
 
 @Component({
   selector: 'app-new-post',
   templateUrl: './new-post.component.html',
   styleUrl: './new-post.component.css',
 })
-export class NewPostComponent {
+export class NewPostComponent implements OnInit {
   permalink: string = '';
   imgSrc: any = './assests/placeholder-image.jpg';
   selectedImg: any;
+
+  categories: Array<object>;
+
+  constructor(private category: CategoriesService) {}
+
+  ngOnInit(): void {
+    this.category.loadData().subscribe((val: any) => {
+      this.categories = val;
+    });
+  }
 
   onTitleChange($event: any) {
     const title = $event.target.value;
